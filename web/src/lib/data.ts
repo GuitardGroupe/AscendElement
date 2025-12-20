cc75a16e7cb165d5ad400fd15c17e18bb083d663
GDev <guitard.groupe@gmail.com>
2025-12-20 14:16:41 +0100

    webp implementation

--- web/src/lib/data.ts
+++ web/src/lib/data.ts
@@ -36,10 +36,10 @@ const mkItem = (
 export const characters = [
     {
         id: 0,
-        symbol: "H",
-        name: "Hydrogen",
-        img: "/images/characters/H.png",
-        img_h: "/images/characters/H_h.png",
+        symbol: "As",
+        name: "Arsenic",
+        img: "/images/webp/As.webp",
+        img_h: "/images/webp/As.webp",
         color: "#00ffff",
         stat_hp: 100, // nombre d epoints de vie max
         stat_energy: 100, // nombre de points d'énergie max
@@ -52,10 +52,10 @@ export const characters = [
     },
     {
         id: 1,
-        symbol: "He",
-        name: "Helium",
-        img: "/images/characters/He.png",
-        img_h: "/images/characters/He_h.png",
+        symbol: "Br",
+        name: "Brome",
+        img: "/images/webp/Br.webp",
+        img_h: "/images/webp/Br.webp",
         color: "#ff9eff",
         stat_hp: 100, // nombre d epoints de vie max
         stat_energy: 100, // nombre de points d'énergie max
@@ -68,10 +68,58 @@ export const characters = [
     },
     {
         id: 2,
+        symbol: "Ga",
+        name: "Gallium",
+        img: "/images/webp/Ga.webp",
+        img_h: "/images/webp/Ga.webp",
+        color: "#F54927",
+        stat_hp: 100, // nombre d epoints de vie max
+        stat_energy: 100, // nombre de points d'énergie max
+        stat_hp_regen: 0, // coef de regen par seconde
+        stat_energy_regen: 10, // coef de regen par seconde
+        stat_attack: 1, // nombre de points de dégats infligés à chaque attaque
+        stat_critical: 0.1, // % de chances de faire un coup critique à chaque attaque
+        stat_haste: 1, // coefficient de rapidité qui vient multiplier les temps de recharge et de lancement
+        stat_def: 0.1, // coefficient de défense qui détermine le % de dégats réduits
+    },
+    {
+        id: 3,
+        symbol: "Kr",
+        name: "Krypton",
+        img: "/images/webp/Kr.webp",
+        img_h: "/images/webp/Kr.webp",
+        color: "#F54927",
+        stat_hp: 100, // nombre d epoints de vie max
+        stat_energy: 100, // nombre de points d'énergie max
+        stat_hp_regen: 0, // coef de regen par seconde
+        stat_energy_regen: 10, // coef de regen par seconde
+        stat_attack: 1, // nombre de points de dégats infligés à chaque attaque
+        stat_critical: 0.1, // % de chances de faire un coup critique à chaque attaque
+        stat_haste: 1, // coefficient de rapidité qui vient multiplier les temps de recharge et de lancement
+        stat_def: 0.1, // coefficient de défense qui détermine le % de dégats réduits
+    },
+    {
+        id: 4,
         symbol: "Li",
         name: "Lithium",
-        img: "/images/characters/Li.png",
-        img_h: "/images/characters/Li_h.png",
+        img: "/images/webp/Li.webp",
+        img_h: "/images/webp/Li.webp",
+        color: "#F54927",
+        stat_hp: 100, // nombre d epoints de vie max
+        stat_energy: 100, // nombre de points d'énergie max
+        stat_hp_regen: 0, // coef de regen par seconde
+        stat_energy_regen: 10, // coef de regen par seconde
+        stat_attack: 1, // nombre de points de dégats infligés à chaque attaque
+        stat_critical: 0.1, // % de chances de faire un coup critique à chaque attaque
+        stat_haste: 1, // coefficient de rapidité qui vient multiplier les temps de recharge et de lancement
+        stat_def: 0.1, // coefficient de défense qui détermine le % de dégats réduits
+    },
+    {
+        id: 5,
+        symbol: "Zn",
+        name: "Zinc",
+        img: "/images/webp/Zn.webp",
+        img_h: "/images/webp/Zn.webp",
         color: "#F54927",
         stat_hp: 100, // nombre d epoints de vie max
         stat_energy: 100, // nombre de points d'énergie max
